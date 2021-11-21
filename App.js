b9e4c21488984976e8d6c9b358513b53d0b7edc2
arivermouth <joensuu.arttu@gmail.com>
2021-11-22 00:33:56 +0200

    first commit

--- App.js
+++ App.js
@@ -1,83 +1,92 @@
-import React from 'react';
-import { StyleSheet, StatusBar, View, Button, TextInput, Text, Alert } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TextInput, View, Button, FlatList } from 'react-native';
+import * as SQLite from 'expo-sqlite';
+
+const db = SQLite.openDatabase('coursedb.db');
 
 export default function App() {
-  const [region, setRegion] = React.useState({
-    latitude: 60.200692,
-    longitude: 24.934302,
-    latitudeDelta: 0.0322,
-    longitudeDelta: 0.0221
-  });
+  const [amount, setAmount] = useState('');
+  const [product, setProduct] = useState('');
+  const [shoppinglist, setShoppinglist] = useState([]);
 
-  const [coordinates, setCoordinates] = React.useState({
-    latitude: 60.201373,
-    longitude: 24.934041
-  });
-
-  const [location, setLocation] = React.useState('');
-  const [spot, setSpot] = React.useState({});
-  const [lat, setLat] = React.useState(0);
-  const [lon, setLon] = React.useState(0);
-
-
-  const getCoordinates = () => {
-    fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=kTvG5ABV5BurZUfD7kK52FMmjbOHPc4V&location=${location}`)
-    .then(response => response.json())
-    .then(responseJson => setSpot(responseJson.results[0].locations[0].latLng))
-    .catch(error => { 
-        Alert.alert('Error', error); 
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql('create table if not exists shoppingitem (id integer primary key not null, amount text, product text);');
     });
-    setLat(Number(spot.lat))
-    setLon(Number(spot.lng))
-    setCoordinates({latitude: lat, longitude: lon})
+    updateList();    
+  }, []);
+
+  // Save shoppingitem
+  const saveItem = () => {
+    db.transaction(tx => {
+        tx.executeSql('insert into shoppingitem (amount, product) values (?, ?);', [amount, product]);    
+      }, null, updateList
+    )
   }
-  
+
+  // Update shoppingitem
+  const updateList = () => {
+    db.transaction(tx => {
+      tx.executeSql('select * from shoppingitem;', [], (_, { rows }) =>
+        setShoppinglist(rows._array)
+      ); 
+    });
+  }
+
+  // Delete shoppingitem
+  const deleteItem = (id) => {
+    db.transaction(
+      tx => {
+        tx.executeSql(`delete from shoppingitem where id = ?;`, [id]);
+      }, null, updateList
+    )    
+  }
+
+  const listSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 5,
+          width: "80%",
+          backgroundColor: "#fff",
+          marginLeft: "10%"
+        }}
+      />
+    );
+  };
 
   return (
     <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={region}
-      >
-        <Marker
-          coordinate={coordinates}
-          title='Haaga-Helia'
-        />
-      </MapView>
-      <TextInput
-        style={styles.input}
-        onChangeText={text => setLocation(text)}
-        placeholder="Syötä sijainti"
-      />
-      <View style={styles.buttons}>
-        <Button
-          title="Näytä"
-          onPress={getCoordinates}
-        />
-      </View>
+      <TextInput placeholder='Product' style={{marginTop: 30, fontSize: 18, width: 200, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(product) => setProduct(product)}
+        value={product}/>  
+      <TextInput placeholder='Amount' style={{ marginTop: 5, marginBottom: 5,  fontSize:18, width: 200, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(amount) => setAmount(amount)}
+        value={amount}/>      
+      <Button onPress={saveItem} title="Save" /> 
+      <Text style={{marginTop: 30, fontSize: 20}}>Shopping list</Text>
+      <FlatList 
+        style={{marginLeft : "5%"}}
+        keyExtractor={item => item.id.toString()} 
+        renderItem={({item}) => <View style={styles.listcontainer}><Text style={{fontSize: 18}}>{item.product}, {item.amount}</Text>
+        <Text style={{fontSize: 18, color: '#0000ff'}} onPress={() => deleteItem(item.id)}> bought</Text></View>} 
+        data={shoppinglist} 
+        ItemSeparatorComponent={listSeparator} 
+      />      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    paddingTop:StatusBar.currentHeight,
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    flex: 1,
-    width: "100%",
-    height: "100%"
-  },
-  input: {
-    width: '100%',
-    padding: 5
-  },
-  buttons: {
-    width: '100%',
-  }
+ container: {
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center',
+ },
+ listcontainer: {
+  flexDirection: 'row',
+  backgroundColor: '#fff',
+  alignItems: 'center'
+ },
 });
